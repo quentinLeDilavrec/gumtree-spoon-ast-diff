a9285d9b799f0ee37446146b777378374fe030ff
quentin <ledilavrecquentin@gmail.com>
2020-02-17 10:10:07 +0100

    better cors

--- src/main/java/gumtree/spoon/AstComparator.java
+++ src/main/java/gumtree/spoon/AstComparator.java
@@ -1,12 +1,19 @@
 package gumtree.spoon;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
+import com.github.gumtreediff.actions.model.Insert;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 
@@ -162,81 +169,130 @@ public class AstComparator {
 		return getCtType(resource);
 	}
 
+	public JsonElement formatDiff(List<Operation> ops) {
+
+		Json4SpoonGenerator x = new Json4SpoonGenerator();
+		// diff.getRootOperations().get(0).getAction().toString();
+
+		JsonObject o = new JsonObject();
+		// o.addProperty(JSON_PROPERTIES.label.toString(), tree.getLabel());
+		// o.addProperty(JSON_PROPERTIES.type.toString(), context.getTypeLabel(tree));
+
+		JsonArray nodeChildens = new JsonArray();
+		o.add("actions", nodeChildens);
+
+		for (Operation tch : ops) {
+			JsonObject y = new JsonObject();
+			y.add("toString", new JsonPrimitive(tch.toString()));
+			// System.out.println(tch.toString());
+			y.add("toJson", tch.toJson());
+			// y.add("name", new JsonPrimitive(tch.getAction().getName()));
+			// y.add("class", new
+			// JsonPrimitive(tch.getAction().getClass().getSimpleName()));
+			// y.add("label", new JsonPrimitive(tch.getAction().getNode().getLabel()));
+			// y.add("srcnode", x.getJSONasJsonObject(tch.getSrcNode()));
+			// y.add("dstnode", x.getJSONasJsonObject(tch.getSrcNode()));
+			nodeChildens.add(y);
+		}
+		return o;
+	}
+
 	public static void main(String[] args) throws Exception {
-
-		port(8087);
-		get("/spoon", (req, res) -> {
-			System.out.println("=========spoon=========");
-			res.header("Content-Type", "application/json");
-			res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
-			res.header("Access-Control-Allow-Credentials", "true");
-			String code = new String(Base64.getDecoder().decode(req.queryParams("code")));
-
-			Json4SpoonGenerator x = new Json4SpoonGenerator();
-			final Diff result = new AstComparator().compare(
-				"",
-				code);
-			// System.out.println(result.getAllOperations().get(0));
-
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-			JsonObject r = x.getJSONasJsonObject(new AstComparator().getCtType(code));
-
-			return gson.toJson(r);
-		});
-		get("/gumtree", (req, res) -> {
-			System.out.println("=========gumtree=========");
-			res.header("Content-Type", "application/json");
-			res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
-			res.header("Access-Control-Allow-Credentials", "true");
-			String old = new String(Base64.getDecoder().decode(req.queryParams("old")));
-			String neww = new String(Base64.getDecoder().decode(req.queryParams("new")));
-
-			Json4SpoonGenerator x = new Json4SpoonGenerator();
-			final Diff diff = new AstComparator().compare(
-				old,
-				neww);
-			// System.out.println(old);
-			// System.out.println(neww);
-			// System.out.println(diff.getAllOperations().get(0));
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-			// diff.getRootOperations().get(0).getAction().toString();
-
-			JsonObject o = new JsonObject();
-			// o.addProperty(JSON_PROPERTIES.label.toString(), tree.getLabel());
-			// o.addProperty(JSON_PROPERTIES.type.toString(), context.getTypeLabel(tree));
-
-			JsonArray nodeChildens = new JsonArray();
-			o.add("actions", nodeChildens);
-
-			for (Operation tch : diff.getRootOperations()) {
-			    JsonObject y = new JsonObject();
-				y.add("toString", new JsonPrimitive(tch.getAction().toString()));
-				y.add("name", new JsonPrimitive(tch.getAction().getName()));
-				y.add("class", new JsonPrimitive(tch.getAction().getClass().toString()));
-				y.add("label", new JsonPrimitive(tch.getAction().getNode().getLabel()));
-				y.add("srcnode", x.getJSONasJsonObject(tch.getSrcNode()));
-				y.add("dstnode", x.getJSONasJsonObject(tch.getSrcNode()));
-				nodeChildens.add(y);
+		Set<String> validOrigins = new HashSet<String>();
+		validOrigins.add("http://127.0.0.1:8081");
+		validOrigins.add("http://localhost:8081");
+		int serverport = 8087;
+		AstComparator comp = new AstComparator();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		Json4SpoonGenerator x = new Json4SpoonGenerator();
+		if (args.length <= 1 || Objects.equals(args[0], "server")) {
+			if ((args.length == 1 && !Objects.equals(args[0], "server")) || args.length > 1) {
+				serverport = Integer.valueOf(args[1]);
 			}
-			System.out.println(diff);
-			return gson.toJson(o);
-		});
-		// internalServerError((req, res) -> {
-		// 	System.out.println("Error");
-		// 	System.out.println(req);
-		// 	System.out.println(res);
-		// 	res.type("application/json");
-		// 	return "{\"message\":\"Custom 500 handling\"}";
-		// });
-		exception(Exception.class, (exception, request, response) -> {
-			// Handle the exception here
-			System.out.println("Exception");
-			System.out.println(exception);
-			System.out.println(request);
-			System.out.println(response);
-		});
+			System.out.println("Launching server on " + serverport);
+			port(serverport);
+			get("/spoon", (req, res) -> {
+				System.out.println("=========spoon=========");
+				res.header("Content-Type", "application/json");
+				if (validOrigins.contains(req.headers("Origin"))) {
+					res.header("Access-Control-Allow-Origin", req.headers("Origin"));
+				}
+				res.header("Access-Control-Allow-Credentials", "true");
+			
+				// // Make sure it's a valid origin
+				// if (validOrigins. (req.origin) != -1) {
+				// 	// Set the header to the requested origin 
+				// 	res.header('Access-Control-Allow-Origin', origin);
+				// }      
+				// // res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080"); // TODO accept more than one origin
+				String code = new String(Base64.getDecoder().decode(req.queryParams("code")));
+
+				JsonObject r = x.getJSONasJsonObject(comp.getCtType(code));
+
+				return gson.toJson(r);
+			});
+			get("/gumtree", (req, res) -> {
+				System.out.println("=========gumtree=========");
+				res.header("Content-Type", "application/json");
+				if (validOrigins.contains(req.headers("Origin"))) {
+					res.header("Access-Control-Allow-Origin", req.headers("Origin"));
+				}
+				res.header("Access-Control-Allow-Credentials", "true");
+				String old = new String(Base64.getDecoder().decode(req.queryParams("old")));
+				String neww = new String(Base64.getDecoder().decode(req.queryParams("new")));
+
+				final Diff diff = comp.compare(old, neww);
+				// System.out.println(old);
+				// System.out.println(neww);
+				// System.out.println(diff.getAllOperations().get(0));
+				JsonElement o = comp.formatDiff(diff.getRootOperations());
+				System.out.println(o);
+				return gson.toJson(o);
+			});
+			// internalServerError((req, res) -> {
+			// System.out.println("Error");
+			// System.out.println(req);
+			// System.out.println(res);
+			// res.type("application/json");
+			// return "{\"message\":\"Custom 500 handling\"}";
+			// });
+			exception(Exception.class, (exception, request, response) -> {
+				// Handle the exception here
+				System.out.println("Exception");
+				System.out.println(exception);
+				System.out.println(request);
+				System.out.println(response);
+			});
+		} else if(Objects.equals(args[0],"cli")){
+			File fl = new File(args[1]);
+			if (args.length==2) {
+
+				JsonObject r = x.getJSONasJsonObject(comp.getCtType(fl));
+				System.out.println(gson.toJson(r));
+			} else {
+				File fr = new File(args[2]);
+
+				final Diff diff = comp.compare(fl, fr);
+				// System.out.println(diff);
+				// System.out.println(neww);
+				// System.out.println(diff.getAllOperations().get(0));
+				List<Operation> ops = new ArrayList<Operation>();
+				for (Operation tch : diff.getRootOperations()) {
+					if (tch.getAction() instanceof Insert) {
+						ops.add(tch);					
+					}
+				}
+				JsonElement o = comp.formatDiff(ops);
+				// System.out.println(gson.toJson(o.getAsJsonObject().get("actions").getAsJsonArray().get(0)));
+				System.out.println(gson.toJson(o));
+			}
+		}
 	}
 
 }
+
+
+
+
+
+
